0d2096f01908f84c98513d24e51f6c6428e68698
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2025-01-08 16:52:18 +0100

    Created and implemented

---
+++ src/main/java/org/example/airlinechatbotbackend/exception/BookingServiceException.java
@@ -0,0 +1,8 @@
+package org.example.airlinechatbotbackend.exception;
+
+public class BookingServiceException extends RuntimeException{
+
+    public BookingServiceException(String message) {
+        super(message);
+    }
+}
