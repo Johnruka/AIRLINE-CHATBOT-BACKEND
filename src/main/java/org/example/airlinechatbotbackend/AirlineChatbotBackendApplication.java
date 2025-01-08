9ac26e829e8ba87956e285f8a63dc3c435455739
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2025-01-08 15:31:28 +0100

    Initial commit

---
+++ src/main/java/org/example/airlinechatbotbackend/AirlineChatbotBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.airlinechatbotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineChatbotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineChatbotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/airlinechatbotbackend/AirlineChatbotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.airlinechatbotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineChatbotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
