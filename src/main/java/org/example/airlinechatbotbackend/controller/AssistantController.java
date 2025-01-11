1eec83d73a38aa9f2eb620e2d2c46035b643122c
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2025-01-11 14:58:55 +0100

    modified

--- src/main/java/org/example/airlinechatbotbackend/controller/AssistantController.java
+++ src/main/java/org/example/airlinechatbotbackend/controller/AssistantController.java
@@ -2,14 +2,12 @@ package org.example.airlinechatbotbackend.controller;
 
 import org.example.airlinechatbotbackend.Service.AssistantServiceImpl;
 import org.example.airlinechatbotbackend.domain.dto.ChatRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/assistant")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AssistantController {
 
     private final AssistantServiceImpl agent;
